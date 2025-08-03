6f28300e3bc26584bd016b6807bb5dea507dcd96
Edinei Rodrigues Filho <edineibk@gmail.com>
2025-08-02 22:35:57 -0300

    💄 style(main): Add `Card` components and improve layout structure
    - Added multiple `Card` components for displaying content in the `Home` page.
    - Enhanced `Home` layout with padding and flex container for better visual alignment.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,17 @@
+import { Card } from '@/ui/components/data-display/card/card';
 import React from 'react';
 
 export default function Home() {
-  return <div className="main-view bg-blue-500 rounded-lg">Main View</div>;
+  return (
+    <div className="main-view rounded-lg p-10">
+      <div className="flex">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
+    </div>
+  );
 }
