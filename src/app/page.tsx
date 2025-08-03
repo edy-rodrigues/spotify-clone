adbd6af50ed8d81f37e05b06cbb5e82e5420ff36
Edinei Rodrigues Filho <edineibk@gmail.com>
2025-08-02 22:55:01 -0300

    💄 style(main): Update `Card` components with improved structure and content display
    - Replaced plain `Card` components with a composed structure utilizing `Root`, `ImageContainer`, `Image`, and `Title` subcomponents.
    - Added sample images and titles for enhanced visual representation.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,60 @@ export default function Home() {
   return (
     <div className="main-view rounded-lg p-10">
       <div className="flex">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        <Card.Root className="basis-1/6">
+          <Card.ImageContainer>
+            <Card.Image
+              src="https://i.scdn.co/image/ab67616d00001e02d6b8ebbddc9b0370281b6465"
+              alt="Taylor Swift"
+            />
+          </Card.ImageContainer>
+          <Card.Title>Ryokuoushoku Shakai</Card.Title>
+        </Card.Root>
+        <Card.Root className="basis-1/6">
+          <Card.ImageContainer>
+            <Card.Image
+              src="https://i.scdn.co/image/ab67616d00001e02d6b8ebbddc9b0370281b6465"
+              alt="Taylor Swift"
+            />
+          </Card.ImageContainer>
+          <Card.Title>Ryokuoushoku Shakai</Card.Title>
+        </Card.Root>
+        <Card.Root className="basis-1/6">
+          <Card.ImageContainer>
+            <Card.Image
+              src="https://i.scdn.co/image/ab67616d00001e02d6b8ebbddc9b0370281b6465"
+              alt="Taylor Swift"
+            />
+          </Card.ImageContainer>
+          <Card.Title>Ryokuoushoku Shakai</Card.Title>
+        </Card.Root>
+        <Card.Root className="basis-1/6">
+          <Card.ImageContainer>
+            <Card.Image
+              src="https://i.scdn.co/image/ab67616d00001e02d6b8ebbddc9b0370281b6465"
+              alt="Taylor Swift"
+            />
+          </Card.ImageContainer>
+          <Card.Title>Ryokuoushoku Shakai</Card.Title>
+        </Card.Root>
+        <Card.Root className="basis-1/6">
+          <Card.ImageContainer>
+            <Card.Image
+              src="https://i.scdn.co/image/ab67616d00001e02d6b8ebbddc9b0370281b6465"
+              alt="Taylor Swift"
+            />
+          </Card.ImageContainer>
+          <Card.Title>Ryokuoushoku Shakai</Card.Title>
+        </Card.Root>
+        <Card.Root className="basis-1/6">
+          <Card.ImageContainer>
+            <Card.Image
+              src="https://i.scdn.co/image/ab67616d00001e02d6b8ebbddc9b0370281b6465"
+              alt="Taylor Swift"
+            />
+          </Card.ImageContainer>
+          <Card.Title>Ryokuoushoku Shakai</Card.Title>
+        </Card.Root>
       </div>
     </div>
   );
