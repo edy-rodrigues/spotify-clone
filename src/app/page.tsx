05923854dbe7a3fdfe1b7b46f60a901fb14047d5
Edinei Rodrigues Filho <edineibk@gmail.com>
2025-08-04 12:15:41 -0300

    ✨ feat(main): Enhance `Home` with playlists and better new releases display.
    - Added `Playlist` component integration for genre-based content display.
    - Used `categoriesForHome` data to render playlists dynamically.
    - Improved `Card.Image` with alt text and size attributes for better accessibility.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
+import { Playlist } from '@/app/_components/playlist';
 import { SpotifyApiFactory } from '@/infra/spotify-api/spotify-api-factory';
+import { categoriesForHome } from '@/server/seed/categories-for-home';
 import { Card } from '@/ui/components/data-display/card/card';
 import React from 'react';
 
 export default async function Home() {
   const spotifyApi = SpotifyApiFactory.create();
 
-  const result = await spotifyApi.browse.getNewReleases({
+  const newReleases = await spotifyApi.browse.getNewReleases({
     country: 'BR',
     limit: 6,
   });
@@ -13,15 +15,19 @@ export default async function Home() {
   return (
     <div className="main-view rounded-lg p-10">
       <div className="flex">
-        {result.albums.items.map((album) => (
+        {newReleases.albums.items.map((album) => (
           <Card.Root className="basis-1/6" key={album.id}>
             <Card.ImageContainer>
-              <Card.Image src={album.images[0].url} alt="Taylor Swift" />
+              <Card.Image src={album.images[0].url} alt="Taylor Swift" sizes="300x300" />
             </Card.ImageContainer>
             <Card.Title>{album.artists.map((artist) => artist.name).join(', ')}</Card.Title>
           </Card.Root>
         ))}
       </div>
+
+      {Object.values(categoriesForHome).map((category) =>
+        Object.values(category.genres).map((genre) => <Playlist key={genre.label} genre={genre} />),
+      )}
     </div>
   );
 }
