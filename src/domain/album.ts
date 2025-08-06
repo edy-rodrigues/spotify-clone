ea4af9b842599ca115829255cb08c8db459b755c
Edinei Rodrigues Filho <edineibk@gmail.com>
2025-08-06 20:33:35 -0300

    ✨ feat(main): Add `Album` domain model for Spotify album data handling.
    - Introduced `Album` class to encapsulate Spotify album data.
    - Added methods to retrieve album ID, name, type, image, and associated artists.
    - Included fallback image for albums without a 64x64 image.

---
+++ src/domain/album.ts
@@ -0,0 +1,33 @@
+import { SimplifiedAlbum as SpotifyAlbum, SimplifiedArtist } from '@spotify/web-api-ts-sdk';
+
+const IMAGE_FALLBACK = 'https://t.scdn.co/images/728ed47fc1674feb95f7ac20236eb6d7.jpeg';
+
+export class Album {
+  private readonly spotifyArtist: SpotifyAlbum;
+
+  public constructor(spotifyArtist: SpotifyAlbum) {
+    this.spotifyArtist = spotifyArtist;
+  }
+
+  public get id(): string {
+    return this.spotifyArtist.id;
+  }
+
+  public get name(): string {
+    return this.spotifyArtist.name;
+  }
+
+  public get image64x64(): string {
+    const imageUrl = this.spotifyArtist.images.find((image) => image.width === 64)?.url;
+
+    return imageUrl || IMAGE_FALLBACK;
+  }
+
+  public get albumType(): string {
+    return this.spotifyArtist.album_type;
+  }
+
+  public get artists(): SimplifiedArtist[] {
+    return this.spotifyArtist.artists;
+  }
+}
