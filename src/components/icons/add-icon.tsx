b7ba010bd3a8604f7ba3f09e399cafbc974af50e
Edinei Rodrigues Filho <edineibk@gmail.com>
2025-08-06 12:42:01 -0300

    ✨ feat(main): Add `AddIcon` component.
    - Introduced reusable SVG component `AddIcon` for consistent iconography.
    - Enabled customization with `SVGProps` and `className` using `cn` utility.

---
+++ src/components/icons/add-icon.tsx
@@ -0,0 +1,21 @@
+import { cn } from '@/utils/cn';
+import React from 'react';
+
+type AddIconProps = React.SVGProps<SVGSVGElement>;
+
+export function AddIcon(props: AddIconProps) {
+  const { className, ...rest } = props;
+
+  return (
+    <svg
+      aria-hidden="true"
+      className={cn(className)}
+      data-encore-id="icon"
+      viewBox="0 0 16 16"
+      {...rest}
+    >
+      <path d="M8 1.5a6.5 6.5 0 1 0 0 13 6.5 6.5 0 0 0 0-13M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8" />
+      <path d="M11.75 8a.75.75 0 0 1-.75.75H8.75V11a.75.75 0 0 1-1.5 0V8.75H5a.75.75 0 0 1 0-1.5h2.25V5a.75.75 0 0 1 1.5 0v2.25H11a.75.75 0 0 1 .75.75" />
+    </svg>
+  );
+}
