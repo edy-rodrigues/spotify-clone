8987ab0cea8bc237c5d2b39e77a5b09f95c7623b
Edinei Rodrigues Filho <edineibk@gmail.com>
2025-08-07 18:56:20 -0300

    ✨ feat(main): Add `TracksOfAlbum` component for displaying album tracks.
    - Implemented `TracksOfAlbum` to fetch and display tracks from a given album using `albumId`.
    - Integrated with carousel for smooth navigation of tracks.
    - Utilized `SpotifyApiFactory` for API interaction and `Pagination` utility for managing results.

---
+++ src/app/(main)/artists/[artistId]/_components/tracks-of-album.tsx
@@ -0,0 +1,45 @@
+import { TrackCard } from '@/app/(main)/artists/[artistId]/_components/track-card';
+import {
+  Carousel,
+  CarouselContent,
+  CarouselNext,
+  CarouselPrevious,
+} from '@/components/ui/carousel';
+import { SpotifyApiFactory } from '@/infra/spotify-api/spotify-api-factory';
+import { Pagination } from '@/utils/pagination';
+import { MaxInt, SimplifiedAlbum } from '@spotify/web-api-ts-sdk';
+import React from 'react';
+
+type TracksOfAlbumProps = Readonly<{
+  album: SimplifiedAlbum;
+}>;
+
+export async function TracksOfAlbum(props: TracksOfAlbumProps) {
+  const { album } = props;
+
+  const spotifyApi = SpotifyApiFactory.create();
+
+  const pagination = await spotifyApi.albums.tracks({
+    albumId: album.id,
+    limit: Pagination.DEFAULT_LIMIT as MaxInt<50>,
+    offset: Pagination.DEFAULT_PAGE,
+  });
+
+  return (
+    <Carousel
+      className="[&>.carousel-content]:-mx-7 [&>.carousel-content]:pr-7"
+      opts={{
+        slidesToScroll: 6,
+        watchDrag: false,
+      }}
+    >
+      <CarouselContent className="m-0 ml-4">
+        {pagination.items.map((track) => (
+          <TrackCard key={track.id} track={track} album={album} />
+        ))}
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
+  );
+}
