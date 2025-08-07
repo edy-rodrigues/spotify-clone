065d81e8ea86949cba53b21f6b73fe29c4931d36
Edinei Rodrigues Filho <edineibk@gmail.com>
2025-08-07 19:49:29 -0300

    ✨ feat(main): Add `BannerSkeleton` component for loading state in the artist page.
    - Implemented `BannerSkeleton` with loading animations to improve user experience.

---
+++ src/app/(main)/artists/[artistId]/_components/loading-components/banner-skeleton.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export function BannerSkeleton() {
+  return (
+    <div className="animate-pulse h-full relative">
+      <div className="absolute left-6 bottom-8 z-20">
+        <span className="flex items-center gap-2">
+          <span className="size-6 bg-gray-400 rounded-full" />
+          <span className="h-4 w-30 bg-gray-400 rounded-lg" />
+        </span>
+        <span className="flex h-16 w-120 bg-gray-400 rounded-lg my-8" />
+        <span className="flex h-4 w-30 bg-gray-400 rounded-lg" />
+        <span className="flex h-4 w-36 bg-gray-400 rounded-lg mt-2" />
+      </div>
+    </div>
+  );
+}
