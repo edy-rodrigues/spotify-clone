070eacfdaf51d084a7d6c9bc6c194818d2905851
Edinei Rodrigues Filho <edineibk@gmail.com>
2025-08-07 19:50:47 -0300

    ♻️ refactor(main): Replace generic fallback with `CarouselSkeleton` in `AlbumsSections` loading state.

--- src/app/(main)/artists/[artistId]/_components/albums-sections.tsx
+++ src/app/(main)/artists/[artistId]/_components/albums-sections.tsx
@@ -1,3 +1,4 @@
+import { CarouselSkeleton } from '@/app/(main)/artists/[artistId]/_components/loading-components/carousel-skeleton';
 import { TracksOfAlbum } from '@/app/(main)/artists/[artistId]/_components/tracks-of-album';
 import { Typography } from '@/components/data-display/typography/typography';
 import { SpotifyApiFactory } from '@/infra/spotify-api/spotify-api-factory';
@@ -25,7 +26,7 @@ export async function AlbumsSections(props: AlbumsSectionsProps) {
           {album.name}
         </Typography>
       </Link>
-      <React.Suspense fallback={<div>Carregando...</div>}>
+      <React.Suspense fallback={<CarouselSkeleton />}>
         <TracksOfAlbum album={album} />
       </React.Suspense>
     </section>
