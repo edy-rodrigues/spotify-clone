f95c823d3352432272bb971b4509dc212a23072d
Edinei Rodrigues Filho <edineibk@gmail.com>
2025-08-07 12:03:35 -0300

    ✨ feat(utils): Add `Pagination` utility class for handling paginated data.
    - Introduced `Pagination` class to calculate pagination details like `currentPage`, `totalPages`, `startItem`, and `endItem`.
    - Includes methods for navigation, such as `hasNext` and `hasPrevious`.

---
+++ src/utils/pagination.ts
@@ -0,0 +1,62 @@
+import { Page } from '@spotify/web-api-ts-sdk';
+
+type Constructor<T = unknown> = {
+  page: Page<T>;
+  currentPage: number;
+  itemsPerPage: number;
+};
+
+type PaginationProps = {
+  constructor: Constructor;
+  props: {
+    page: Page<unknown>;
+    currentPage: number;
+    itemsPerPage: number;
+  };
+};
+
+export class Pagination {
+  public static DEFAULT_PAGE = 20;
+  public static DEFAULT_LIMIT = 1;
+  private readonly props: PaginationProps['props'];
+
+  public constructor(props: PaginationProps['constructor']) {
+    this.props = {
+      page: props.page,
+      currentPage: props.currentPage,
+      itemsPerPage: props.itemsPerPage,
+    };
+  }
+
+  public get itemsPerPage(): number {
+    return this.props.itemsPerPage;
+  }
+
+  public get totalItems(): number {
+    return this.props.page.total;
+  }
+
+  public get totalPages(): number {
+    return Math.ceil(this.totalItems / this.props.itemsPerPage);
+  }
+
+  public get currentPage(): number {
+    return Math.max(1, Math.min(this.props.currentPage, this.totalPages || 1));
+  }
+
+  public get startItem(): number {
+    return (this.currentPage - 1) * this.props.itemsPerPage + 1;
+  }
+
+  public get endItem(): number {
+    return Math.min(this.startItem + this.props.itemsPerPage - 1, this.props.page.total);
+  }
+
+  public get hasNext(): boolean {
+    return this.currentPage < this.totalPages;
+  }
+
+  public get hasPrevious(): boolean {
+    return this.currentPage > 1;
+  }
+}
