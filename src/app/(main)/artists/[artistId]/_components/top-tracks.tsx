1e6bd82a24f8f44bc5164af2ad82db9dd03de58e
Edinei Rodrigues Filho <edineibk@gmail.com>
2025-08-07 13:44:56 -0300

    ♻️ refactor(main): Update `TopTracks` to use `TopTracksCard` and fetch artist's top tracks dynamically.
    - Replaced static new releases data with dynamically fetched top tracks for the given artist ID.
    - Simplified rendering logic by integrating `TopTracksCard` for displaying individual track details.

--- src/app/(main)/artists/[artistId]/_components/top-tracks.tsx
+++ src/app/(main)/artists/[artistId]/_components/top-tracks.tsx
@@ -1,46 +1,35 @@
-import {
-  CardImage,
-  CardImageContainer,
-  CardPlayButton,
-  CardRoot,
-  CardTitle,
-} from '@/components/data-display/card';
+import { TopTracksCard } from '@/app/(main)/artists/[artistId]/_components/top-tracks-card';
 import {
   Carousel,
   CarouselContent,
-  CarouselItem,
   CarouselNext,
   CarouselPrevious,
 } from '@/components/ui/carousel';
 import { SpotifyApiFactory } from '@/infra/spotify-api/spotify-api-factory';
 import React from 'react';
 
-export async function TopTracks() {
+type TopTracksProps = Readonly<{
+  artistId: string;
+}>;
+
+export async function TopTracks(props: TopTracksProps) {
+  const { artistId } = props;
+
   const spotifyApi = SpotifyApiFactory.create();
 
-  const newReleases = await spotifyApi.browse.getNewReleases({
-    country: 'BR',
-    limit: 6,
-  });
-  await new Promise((resolve) => setTimeout(resolve, 3000));
+  const result = await spotifyApi.artists.topTracks(artistId, 'BR');
 
   return (
     <Carousel
+      className="[&>.carousel-content]:-mx-7 [&>.carousel-content]:pr-7"
       opts={{
         slidesToScroll: 6,
+        watchDrag: false,
       }}
     >
-      <CarouselContent>
-        {newReleases.albums.items.map((album) => (
-          <CarouselItem className="basis-1/8" key={album.id}>
-            <CardRoot href={`/albums/${album.id}`}>
-              <CardImageContainer>
-                <CardImage src={album.images[0].url} alt={album.name} sizes="300x300" />
-                <CardPlayButton />
-              </CardImageContainer>
-              <CardTitle>{album.artists.map((artist) => artist.name).join(', ')}</CardTitle>
-            </CardRoot>
-          </CarouselItem>
+      <CarouselContent className="m-0 ml-4">
+        {result.tracks.map((track) => (
+          <TopTracksCard key={track.id} track={track} />
         ))}
       </CarouselContent>
       <CarouselPrevious />
