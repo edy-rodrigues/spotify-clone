d8343090c7f8909a6458b96fc38d5ca6449f2a68
Edinei Rodrigues Filho <edineibk@gmail.com>
2025-08-08 11:41:48 -0300

    ✨ feat(main): Add `NewReleases` component with Spotify API integration for displaying new album releases.

---
+++ src/app/(main)/_components/new-releases.tsx
@@ -0,0 +1,82 @@
+import { CardButton } from '@/app/(main)/_components/card-button';
+import { CardLink } from '@/app/(main)/_components/card-link';
+import {
+  CardImage,
+  CardImageContainer,
+  CardRoot,
+  CardSubtitle,
+  CardTitle,
+} from '@/components/data-display/card';
+import { Typography } from '@/components/data-display/typography/typography';
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from '@/components/ui/carousel';
+import { SpotifyApiFactory } from '@/infra/spotify-api/spotify-api-factory';
+import Link from 'next/link';
+import React from 'react';
+
+export async function NewReleases() {
+  const spotifyApi = SpotifyApiFactory.create();
+
+  const newReleases = await spotifyApi.browse.getNewReleases({
+    country: 'BR',
+  });
+
+  return (
+    <div className="flex flex-col">
+      <Typography variant="h2" className="mb-2 ml-3">
+        <Link href="#" className="hover:underline">
+          Lançamentos
+        </Link>
+      </Typography>
+      <Carousel
+        className="[&>.carousel-content]:-mx-10 [&>.carousel-content]:pr-10"
+        opts={{
+          slidesToScroll: 6,
+          watchDrag: false,
+        }}
+      >
+        <CarouselContent className="m-0 ml-10">
+          {newReleases.albums.items.reverse().map((album) => (
+            <CarouselItem className="basis-1/8 min-w-[177px] p-0" key={album.id}>
+              <CardRoot href={`/albums/${album.id}`} asChild>
+                <CardButton albumId={album.id}>
+                  <CardImageContainer>
+                    <CardImage src={album.images[0].url} alt={album.name} sizes="300x300" />
+                  </CardImageContainer>
+                  <CardTitle>{album.name}</CardTitle>
+                  <CardSubtitle>
+                    <div className="flex items-center flex-wrap">
+                      {album.artists.map((artist, index) => (
+                        <React.Fragment key={artist.id}>
+                          <CardLink
+                            key={artist.id}
+                            href={`/artists/${artist.id}`}
+                            className="hover:underline hover:[&>*]:text-white"
+                          >
+                            <Typography className="text-sm truncate text-text-gray">
+                              {artist.name}
+                            </Typography>
+                          </CardLink>
+                          {index !== album.artists.length - 1 && (
+                            <span className="text-text-gray mr-1">,</span>
+                          )}
+                        </React.Fragment>
+                      ))}
+                    </div>
+                  </CardSubtitle>
+                </CardButton>
+              </CardRoot>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+    </div>
+  );
+}
