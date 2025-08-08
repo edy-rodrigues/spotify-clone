5b9f9de122e225e0c9e78379f7554a8d87654ae9
Edinei Rodrigues Filho <edineibk@gmail.com>
2025-08-08 11:40:39 -0300

    ♻️ refactor(main): Align `AlbumTableRow` artist name layout with `flex items-center` for improved styling consistency.

--- src/app/(main)/artists/[artistId]/_components/album-table-row.tsx
+++ src/app/(main)/artists/[artistId]/_components/album-table-row.tsx
@@ -44,7 +44,7 @@ export function AlbumTableRow(props: AlbumTableRowProps) {
         <Link href="#" className="hover:underline">
           <Typography className="text-base truncate">{album.name}</Typography>
         </Link>
-        <div className="flex">
+        <div className="flex items-center">
           {album.artists.map((artist, index) => (
             <React.Fragment key={artist.id}>
               <Link
