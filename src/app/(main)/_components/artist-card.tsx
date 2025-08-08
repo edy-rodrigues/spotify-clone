ec23ba3a5e6bbd11330990bbc747825f6f43dcf2
Edinei Rodrigues Filho <edineibk@gmail.com>
2025-08-08 11:41:24 -0300

    ♻️ refactor(main): Replace `Card` imports in `ArtistCard` with named imports for improved readability and consistency.

--- src/app/(main)/_components/artist-card.tsx
+++ src/app/(main)/_components/artist-card.tsx
@@ -1,6 +1,12 @@
+import {
+  CardImage,
+  CardImageContainer,
+  CardRoot,
+  CardSubtitle,
+  CardTitle,
+} from '@/components/data-display/card';
 import { CarouselItem } from '@/components/ui/carousel';
 import { Artist } from '@/domain/artist';
-import { Card } from '@/components/data-display/card';
 import React from 'react';
 
 type ArtistCardProps = Readonly<{
@@ -12,18 +18,18 @@ export async function ArtistCard(props: ArtistCardProps) {
 
   return (
     <CarouselItem className="basis-1/8 min-w-[177px] p-0">
-      <Card.Root href={`/artists/${artist.id}`} key={artist.id}>
-        <Card.ImageContainer>
-          <Card.Image
+      <CardRoot href={`/artists/${artist.id}`} key={artist.id}>
+        <CardImageContainer>
+          <CardImage
             className="rounded-full"
             src={artist.image320x320}
             alt={artist.name}
             sizes="320x320"
           />
-        </Card.ImageContainer>
-        <Card.Title>{artist.name}</Card.Title>
-        <Card.Subtitle>Artista</Card.Subtitle>
-      </Card.Root>
+        </CardImageContainer>
+        <CardTitle>{artist.name}</CardTitle>
+        <CardSubtitle>Artista</CardSubtitle>
+      </CardRoot>
     </CarouselItem>
   );
 }
