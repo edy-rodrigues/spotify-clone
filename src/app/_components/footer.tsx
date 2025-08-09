1e13e71d73548ad7242c19fed86eca77e0b441b7
Edinei Rodrigues Filho <edineibk@gmail.com>
2025-08-09 05:11:42 -0300

    ✨ feat(main): Enhance footer with new layout and componets.
    Redesign footer layout with gradient background and flexbox. Add `Typography` for text and `Button` for call-to-action, improving style and functionality.

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -1,5 +1,22 @@
+import { Typography } from '@/components/data-display/typography';
+import { Button } from '@/components/ui/button';
 import React from 'react';
 
 export function Footer() {
-  return <footer>Footer</footer>;
+  return (
+    <footer className="flex justify-between items-center pt-[11px] pb-[7px] pl-[14] pr-6 bg-[linear-gradient(90deg,#af2896,#509bf5)]">
+      <div>
+        <Typography variant="h6" className="text-sm">
+          Testar o Premium de graça
+        </Typography>
+        <Typography variant="body1" className="text-base">
+          Inscreva-se para curtir música ilimitada e podcasts só com alguns anúncios. Não precisa de
+          cartão de crédito.
+        </Typography>
+      </div>
+      <Button variant="default" size="lg">
+        Inscreva-se grátis
+      </Button>
+    </footer>
+  );
 }
