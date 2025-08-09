96482cc23b8814de5691a6519db8d1e9a792dfe9
Edinei Rodrigues Filho <edineibk@gmail.com>
2025-08-08 22:26:53 -0300

    ♻️ refactor(main): Replace `Button` tabs with `Carousel` and `ToggleGroup` for enhanced usability and layout efficiency.

--- src/app/(main)/search/[term]/page.tsx
+++ src/app/(main)/search/[term]/page.tsx
@@ -1,33 +1,69 @@
-import { Button } from '@/components/ui/button';
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from '@/components/ui/carousel';
+import { ToggleGroup, ToggleGroupItem } from '@/components/ui/toggle-group';
 
 export default function SearchPage() {
   return (
     <div className="main-view rounded-lg min-h-full flex-1 before:content-[none] p-6">
       <div className="flex items-center gap-3 flex-wrap">
-        <Button variant="default" size="sm">
-          Tudo
-        </Button>
-        <Button variant="secondary" size="sm" disabled>
-          Músicas
-        </Button>
-        <Button variant="secondary" size="sm">
-          Artistas
-        </Button>
-        <Button variant="secondary" size="sm" disabled>
-          Playlists
-        </Button>
-        <Button variant="secondary" size="sm">
-          Álbuns
-        </Button>
-        <Button variant="secondary" size="sm" disabled>
-          Podcasts e programas
-        </Button>
-        <Button variant="secondary" size="sm" disabled>
-          Gêneros e momentos
-        </Button>
-        <Button variant="secondary" size="sm" disabled>
-          Perfis
-        </Button>
+        <Carousel
+          className="w-full"
+          opts={{
+            dragFree: true,
+          }}
+        >
+          <ToggleGroup type="single" defaultValue="all" asChild>
+            <CarouselContent className="m-0 gap-2">
+              <CarouselItem className="basis-auto p-0">
+                <ToggleGroupItem value="all" size="sm">
+                  Tudo
+                </ToggleGroupItem>
+              </CarouselItem>
+              <CarouselItem className="basis-auto p-0">
+                <ToggleGroupItem value="artists" size="sm">
+                  Artistas
+                </ToggleGroupItem>
+              </CarouselItem>
+              <CarouselItem className="basis-auto p-0">
+                <ToggleGroupItem value="albums" size="sm">
+                  Álbuns
+                </ToggleGroupItem>
+              </CarouselItem>
+              <CarouselItem className="basis-auto p-0">
+                <ToggleGroupItem value="musics" size="sm" disabled>
+                  Músicas
+                </ToggleGroupItem>
+              </CarouselItem>
+              <CarouselItem className="basis-auto p-0">
+                <ToggleGroupItem value="playlists" size="sm" disabled>
+                  Playlists
+                </ToggleGroupItem>
+              </CarouselItem>
+              <CarouselItem className="basis-auto p-0">
+                <ToggleGroupItem value="shows" size="sm" disabled>
+                  Podcasts e programas
+                </ToggleGroupItem>
+              </CarouselItem>
+              <CarouselItem className="basis-auto p-0">
+                <ToggleGroupItem value="videos" size="sm" disabled>
+                  Gêneros e momentos
+                </ToggleGroupItem>
+              </CarouselItem>
+              <CarouselItem className="basis-auto p-0">
+                <ToggleGroupItem value="users" size="sm" disabled>
+                  Perfis
+                </ToggleGroupItem>
+              </CarouselItem>
+            </CarouselContent>
+          </ToggleGroup>
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
       </div>
     </div>
   );
