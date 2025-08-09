786ba7abbce2abab727e97e3eb51d390ccf26236
Edinei Rodrigues Filho <edineibk@gmail.com>
2025-08-09 03:25:15 -0300

    ✨ feat(main): Add `CarouselAlbumItem` component for album display in `Carousel`.

---
+++ src/components/carousel/implementations/carousel-album-item.tsx
@@ -0,0 +1,57 @@
+import { CarouselAlbumItemButton } from '@/components/carousel/implementations/carousel-album-item-button';
+import {
+  CardImage,
+  CardImageContainer,
+  CardLink,
+  CardRoot,
+  CardSubtitle,
+  CardTitle,
+} from '@/components/data-display/card';
+import { Typography } from '@/components/data-display/typography';
+import { CarouselItem } from '@/components/ui/carousel';
+import { Album } from '@/domain/album';
+import { SimplifiedAlbum } from '@spotify/web-api-ts-sdk';
+import React from 'react';
+
+type CarouselAlbumItemProps = Readonly<{
+  album: SimplifiedAlbum;
+}>;
+
+export function CarouselAlbumItem(props: CarouselAlbumItemProps) {
+  const { album: spotifyAlbum } = props;
+
+  const album = new Album(spotifyAlbum);
+
+  return (
+    <CarouselItem className="basis-1/8 min-w-[177px] p-0" key={album.id}>
+      <CardRoot href={`/albums/${album.id}`} asChild>
+        <CarouselAlbumItemButton albumId={album.id}>
+          <CardImageContainer>
+            <CardImage src={album.image300x300} alt={album.name} sizes="300x300" />
+          </CardImageContainer>
+          <CardTitle>{album.name}</CardTitle>
+          <CardSubtitle>
+            <div className="flex items-center flex-wrap">
+              {album.artists.map((artist, index) => (
+                <React.Fragment key={artist.id}>
+                  <CardLink
+                    key={artist.id}
+                    href={`/artists/${artist.id}`}
+                    className="hover:underline hover:[&>*]:text-white"
+                  >
+                    <Typography className="text-sm truncate text-text-gray">
+                      {artist.name}
+                    </Typography>
+                  </CardLink>
+                  {index !== album.artists.length - 1 && (
+                    <span className="text-text-gray mr-1">,</span>
+                  )}
+                </React.Fragment>
+              ))}
+            </div>
+          </CardSubtitle>
+        </CarouselAlbumItemButton>
+      </CardRoot>
+    </CarouselItem>
+  );
+}
