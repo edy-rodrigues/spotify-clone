1276f14cde06f6e3e81150bf01ce8de315d330bc
Edinei Rodrigues Filho <edineibk@gmail.com>
2025-08-10 19:48:47 -0300

    ✨ feat(main): Localize `InitialSearchPage` with `next-intl`.
    Add localization support for `InitialSearchPage` using `next-intl`. Replace static text with translated content and update `pt-BR` messages with new keys for page title and description.

--- src/app/[locale]/search/page.tsx
+++ src/app/[locale]/search/page.tsx
@@ -1,14 +1,17 @@
 import { SearchInput } from '@/app/_components/search-input';
 import { Typography } from '@/components/data-display/typography';
 import { ScrollArea } from '@/components/ui/scroll-area';
+import { getTranslations } from 'next-intl/server';
 import React from 'react';
 
 export default async function InitialSearchPage() {
+  const t = await getTranslations();
+
   return (
     <ScrollArea className="main-view rounded-lg flex-1 before:content-[none] relative p-2">
       <div className="flex flex-col min-h-full mt-4 p-2">
         <Typography variant="h1" className="mb-4">
-          Buscar
+          {t('initialSearchPage.title')}
         </Typography>
         <SearchInput />
 
@@ -16,7 +19,7 @@ export default async function InitialSearchPage() {
           variant="h4"
           className="absolute top-0 bottom-0 left-0 right-0 m-auto text-xl h-fit text-center max-w-[250px] text-text-gray"
         >
-          Pesquisa e encontre seus artistas favoritos.
+          {t('initialSearchPage.description')}
         </Typography>
       </div>
     </ScrollArea>
