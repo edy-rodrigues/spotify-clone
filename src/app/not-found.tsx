7cc6792fd1e4fb2bb5fc79bbb7a7cd736605cc9e
Edinei Rodrigues Filho <edineibk@gmail.com>
2025-08-10 21:59:30 -0300

    ✨ feat(main): Localize `NotFound` page and update translations.
    Add `next-intl` localization to the `NotFound` page. Replace static text with dynamic translations using `getTranslations`. Update `pt-BR`, `en`, and `kr` message files with new keys for `notFoundPage`.

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,27 +3,30 @@ import { SpotifyIcon } from '@/components/icons/spotify-icon';
 import { Button } from '@/components/ui/button';
 import type { Metadata } from 'next';
 import { Link } from '@/i18n/navigation';
+import { getTranslations } from 'next-intl/server';
 
 export const metadata: Metadata = {
   title: 'Página não encontrada',
   description: 'O Spotify é um serviço de música digital que dá acesso a milhões de músicas.',
 };
 
-export default function NotFound() {
+export default async function NotFound() {
+  const t = await getTranslations();
+
   return (
-    <div className="fixed h-dvh w-full bg-background-elevation-1 flex flex-col justify-center items-center overflow-hidden">
+    <div className="fixed h-full w-full bg-background-elevation-1 flex flex-col justify-center items-center overflow-hidden">
       <SpotifyIcon className="fill-primary-main w-15 h-15 mb-10" />
       <Typography variant="h1" className="mb-4 min-md:text-5xl">
-        Página não encontrada
+        {t('notFoundPage.title')}
       </Typography>
       <Typography variant="body1" className="text-text-gray text-base">
-        Não encontramos a página que você queria.
+        {t('notFoundPage.description')}
       </Typography>
       <Button className="mt-10" size="lg" asChild>
-        <Link href="/">Início</Link>
+        <Link href="/">{t('notFoundPage.homeLink')}</Link>
       </Button>
       <Button className="mt-6" size="lg" variant="link" asChild>
-        <Link href="/">Ajuda</Link>
+        <Link href="/">{t('notFoundPage.helpLink')}</Link>
       </Button>
     </div>
   );
